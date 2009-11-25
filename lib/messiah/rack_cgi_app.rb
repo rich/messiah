4ae1a76d5d9967bd09cbb5b6094c95432ce870a6
Rich Cavanaugh <rich@Rich-Cavanaughs-MacBook-Pro.local>
2009-11-25 18:30:42 -0500

    refactor into a real ruby library

--- lib/messiah/rack_cgi_app.rb
+++ lib/messiah/rack_cgi_app.rb
@@ -2,17 +2,10 @@ require 'open3'
 
 class Messiah::RackCGIApp
   def call(env)
-    env['HTTP_HOST'] = Messiah.host
-    env['DOCUMENT_ROOT'] = Messiah.root
-    env['SCRIPT_NAME'] = (Messiah.script || env['PATH_INFO']).gsub(/^\//, '')
-    env['SCRIPT_FILENAME'] = env['PATH_TRANSLATED'] = File.join(env['DOCUMENT_ROOT'], env['SCRIPT_NAME'])
-
-    env_strings = env.inject([]) do |a, item|
-      a << "#{item.first}=\"#{item.last}\""
-      a
-    end
+    add_to_environment!(env)
+    env_string = build_environment_string(env)
     
-    stdin, stdout, stderr = Open3.popen3("env #{env_strings.join(' ')} #{Messiah.command}")
+    stdin, stdout, stderr = Open3.popen3("env #{env_string} #{Messiah.command}")
     response = stdout.read
     # puts stderr.read
     header, body = response.split("\n\r", 2)
@@ -24,5 +17,19 @@ class Messiah::RackCGIApp
     end
     status = headers.delete('Status') || 200
     [status, headers, [body]]
+  end
+  
+  def add_to_environment!(env)
+    env['HTTP_HOST'] = Messiah.host
+    env['DOCUMENT_ROOT'] = Messiah.root
+    env['SCRIPT_NAME'] = (Messiah.script || env['PATH_INFO']).gsub(/^\//, '')
+    env['SCRIPT_FILENAME'] = env['PATH_TRANSLATED'] = File.join(env['DOCUMENT_ROOT'], env['SCRIPT_NAME'])
+  end
+  
+  def build_environment_string(env)
+    env.inject([]) do |a, item|
+      a << "#{item.first}=\"#{item.last}\""
+      a
+    end.join(' ')
   end
 end
